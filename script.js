97758d426c651b8ae403959a706d367b7e8cbcec
Med Reda Kamal <gencokm@gmail.com>
2022-02-03 23:20:37 +0100

    Tip 1 : Iterate with for...of loop using Symbol.iterator - Users Example

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
  * Master Javascript - by Med Reda Kamal
  * Script File
  */
+
+/* Tip 1 : Iterate with for...of loop using Symbol.iterator - (Users Example) */
+const usersArr = [
+  { name: "Kamal", email: "kamal@gmail.com", status: "Active" },
+  { name: "Ilham", email: "ilham@gmail.com", status: "Not active" },
+  { name: "Youssef", email: "youssef@gmail.com", status: "Active" },
+];
+const usersArrIterator = usersArr[Symbol.iterator]();
+const usersListDOM = document.getElementById("usersList");
+
+for (let user of usersArrIterator) {
+  const userRow = document.createElement("tr");
+  const userNameDOM = document.createElement("td");
+  const userEmailDOM = document.createElement("td");
+  const userStatusDOM = document.createElement("td");
+
+  userNameDOM.textContent = user.name;
+  userEmailDOM.textContent = user.email;
+  userStatusDOM.textContent = user.status;
+
+  userRow.appendChild(userNameDOM);
+  userRow.appendChild(userEmailDOM);
+  userRow.appendChild(userStatusDOM);
+
+  usersListDOM.appendChild(userRow);
+}
